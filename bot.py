13e3e54eca8ebe40cd19864ff6f340eacec8fa0b
HappyTW <49824609+HappyTW@users.noreply.github.com>
2021-09-23 16:50:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = commands.Bot(os.environ['prefix'])
 @bot.event
 async def on_ready():
     print('澳門首家ㄐㄐ賭場上線啦')
-    await bot.change_presence(activity = discord.Streaming(name = "!help | !check進行身分認證", url = "https://www.twitch.tv/"))
+    await bot.change_presence(activity = discord.Streaming(name = "!help | !check進行身分認證", url = "https://www.twitch.tv/directory/game/Always%20On"))
 
 for fileName in os.listdir('./commands'):
     if fileName.endswith('.py'):
