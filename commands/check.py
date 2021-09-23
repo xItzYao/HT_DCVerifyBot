e6cf5b9e367b0945a39d6a5f94ce92bb9d37d86c
HappyTW <49824609+HappyTW@users.noreply.github.com>
2021-09-23 17:05:10 +0800

    Update check.py

--- commands/check.py
+++ commands/check.py
@@ -11,68 +11,73 @@ from difflib import *
 
 class Check(Cog_Extension):
     @commands.command()
-    async def check(self, ctx):
+    async def check(self, ctx, mcid):
         await ctx.send(f"<@!{ctx.message.author.id}> Checking......")
         nickname = str(ctx.message.author.display_name)
         if SequenceMatcher(None, nickname, "<>").ratio() == 0:
-            await ctx.send("暱稱格式不正確\n請改為暱稱<Minecraft ID>")
+            await ctx.send("暱稱格式不正確\n請改為暱稱<Minecraft ID>\n如果你沒有Mineccraft ID 請輸入!check noid")
         else:
-            id = nickname.split('<')
-            id1 = id[1].split('>')
-            await ctx.send(f"Minecraft ID : {id1[0]}")
-            r2 = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{id1[0]}")
-            print(r2.status_code)
-            if r2.status_code == 200:
-                pName = r2.json()
-                r = requests.get(f"https://api.hypixel.net/findGuild?key={os.environ['API_KEY']}&byUuid={pName['id']}")
-                gid = r.json()
-                if gid['guild'] is None:
-                    await ctx.send('查無公會')
-                else:
-                    r1 = requests.get(f"https://api.hypixel.net/guild?key={os.environ['API_KEY']}&id={gid['guild']}")
-                    gjson = r1.json()
-                    await ctx.send(f"目前所在公會 : {gjson['guild']['name']}")
+            if mcid == "noid":
+                pRank = "None"
             else:
-                await ctx.send('查無此ID')
-            r3 = requests.get(f"https://api.hypixel.net/player?key={os.environ['API_KEY']}&uuid={pName['id']}")
-            playerRank = r3.json()
-            if "rank" in playerRank['player']:
-                pRank = playerRank['player']['rank']
-            else:
-                if "monthlyPackageRank" in playerRank['player']:
-                    pRank = "MVP_PLUS_PLUS"
-                else:
-                    if "newPackageRank" in playerRank['player']:
-                        pRank = playerRank['player']['newPackageRank']
+                id = nickname.split('<')
+                id1 = id[1].split('>')
+                await ctx.send(f"Minecraft ID : {id1[0]}")
+                r2 = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{id1[0]}")
+                print(r2.status_code)
+                if r2.status_code == 200:
+                    pName = r2.json()
+                    r = requests.get(f"https://api.hypixel.net/findGuild?key={os.environ['API_KEY']}&byUuid={pName['id']}")
+                    gid = r.json()
+                    if gid['guild'] is None:
+                        await ctx.send('查無公會')
                     else:
-                        pRank = "None"
-            await ctx.send(f"Hypixel Rank : {pRank}")
-            if pRank == "MVP_PLUS_PLUS":
-                memberRole = discord.utils.get(ctx.guild.roles, name='MVP++')
-            elif pRank == "MVP_PLUS":
-                memberRole = discord.utils.get(ctx.guild.roles, name='MVP+')
-            elif pRank == "MVP":
-                memberRole = discord.utils.get(ctx.guild.roles, name='MVP')
-            elif pRank == "VIP+":
-                memberRole = discord.utils.get(ctx.guild.roles, name='VIP+')
-            elif pRank == "VIP":
-                memberRole = discord.utils.get(ctx.guild.roles, name='VIP')
-            elif pRank == "None":
-                memberRole = discord.utils.get(ctx.guild.roles, name='none')
-            elif pRank == "ADMIN":
-                memberRole = discord.utils.get(ctx.guild.roles, name='Hypixel Admin')
-            elif pRank == "YOUTUBER":
-                memberRole = discord.utils.get(ctx.guild.roles, name='Hypixel Youtuber')
-            else:
-                await ctx.send("查無Rank，請聯絡管理員:(")
-                memberRole = discord.utils.get(ctx.guild.roles, name='none')
-            print(memberRole)
-            await ctx.author.add_roles(memberRole)
-            await ctx.send("Hypixel Rank身分組成功增加")
+                        r1 = requests.get(f"https://api.hypixel.net/guild?key={os.environ['API_KEY']}&id={gid['guild']}")
+                        gjson = r1.json()
+                        await ctx.send(f"目前所在公會 : {gjson['guild']['name']}")
+                else:
+                    await ctx.send('查無此ID')
+                r3 = requests.get(f"https://api.hypixel.net/player?key={os.environ['API_KEY']}&uuid={pName['id']}")
+                playerRank = r3.json()
+                if "rank" in playerRank['player']:
+                    pRank = playerRank['player']['rank']
+                else:
+                    if "monthlyPackageRank" in playerRank['player']:
+                        pRank = "MVP_PLUS_PLUS"
+                    else:
+                        if "newPackageRank" in playerRank['player']:
+                            pRank = playerRank['player']['newPackageRank']
+                        else:
+                            pRank = "None"
+                await ctx.send(f"Hypixel Rank : {pRank}")
+                if pRank == "MVP_PLUS_PLUS":
+                    memberRole = discord.utils.get(ctx.guild.roles, name='MVP++')
+                elif pRank == "MVP_PLUS":
+                    memberRole = discord.utils.get(ctx.guild.roles, name='MVP+')
+                elif pRank == "MVP":
+                    memberRole = discord.utils.get(ctx.guild.roles, name='MVP')
+                elif pRank == "VIP+":
+                    memberRole = discord.utils.get(ctx.guild.roles, name='VIP+')
+                elif pRank == "VIP":
+                    memberRole = discord.utils.get(ctx.guild.roles, name='VIP')
+                elif pRank == "None":
+                    memberRole = discord.utils.get(ctx.guild.roles, name='none')
+                elif pRank == "ADMIN":
+                    memberRole = discord.utils.get(ctx.guild.roles, name='Hypixel Admin')
+                elif pRank == "YOUTUBER":
+                    memberRole = discord.utils.get(ctx.guild.roles, name='Hypixel Youtuber')
+                else:
+                    await ctx.send("查無Rank，請聯絡管理員:(")
+                    memberRole = discord.utils.get(ctx.guild.roles, name='none')
+                print(memberRole)
+                await ctx.author.add_roles(memberRole)
+                await ctx.send("Hypixel Rank身分組成功增加")
             if gid['guild'] is None:
                 memberRoleG = discord.utils.get(ctx.guild.roles, name='🌈好捧油🌈<Friend>')
             elif gjson['guild']['name'] == "HelloTaiwan":
                 memberRoleG = discord.utils.get(ctx.guild.roles, name='普通會員<Member>')
+            elif noid == "noid":
+                memberRoleG = discord.utils.get(ctx.guild.roles, name='🌈好捧油🌈<Friend>')            
             else:
                 memberRoleG = discord.utils.get(ctx.guild.roles, name='🌈好捧油🌈<Friend>')
             await ctx.author.add_roles(memberRoleG)
