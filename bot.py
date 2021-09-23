20f00bb426082ab3b278ba2364107a9be6e81207
HappyTW <49824609+HappyTW@users.noreply.github.com>
2021-09-23 16:21:07 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import json
 import os
 from discord.ext import commands
 
-activity = discord.Game(name="!help")
+activity = discord.Streaming(name="!help | !check進行身分認證", url="https://www.twitch.tv/")
 bot = commands.Bot(os.environ['prefix'], activity=activity, status=discord.Status.idle)
 
 @bot.event
