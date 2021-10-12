1eb87da3a471eab731b8b2930d56f455a526bfb1
HappyTW <49824609+HappyTW@users.noreply.github.com>
2021-10-12 20:41:05 +0800

    Update hypixel.py

--- core/hypixel.py
+++ core/hypixel.py
@@ -11,8 +11,6 @@ import grequests
 
 import leveling
 
-response = {}
-
 HYPIXEL_API_URL = 'https://api.hypixel.net/'
 UUIDResolverAPI = "https://sessionserver.mojang.com/session/minecraft/profile/"
 
