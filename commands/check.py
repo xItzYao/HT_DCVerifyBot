438e653e90cfe3c849c7545bc1d866bcbbeaba02
HappyTW <49824609+HappyTW@users.noreply.github.com>
2021-10-13 15:51:13 +0800

    Update check.py

--- commands/check.py
+++ commands/check.py
@@ -47,7 +47,8 @@ class Test(Cog_Extension):
                           print(playerGuildName)
                       except:
                           print(playerGuildName)
-                          await ctx.guild.create_role(name = playerGuildName)
+                          guild = bot.get_guild(518290953908518922)
+                          await guild.create_role(name = playerGuildName)
                           print(playerGuildName)
                           memberGuildRole = discord.utils.get(ctx.guild.roles, name=playerGuildName)
                       await ctx.author.add_roles(memberGuildRole)
