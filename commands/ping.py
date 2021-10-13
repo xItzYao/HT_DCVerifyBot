db26d3a69bb4ebe09f1ccde7fd3de7d5d58ed689
HappyTW <49824609+HappyTW@users.noreply.github.com>
2021-10-13 17:59:55 +0800

    Update ping.py

--- commands/ping.py
+++ commands/ping.py
@@ -5,6 +5,7 @@ from core.classes import Cog_Extension
 class Ping(Cog_Extension):
     @commands.command()
     async def ping(self,ctx):
+        await ctx.guild.create_role(name = 'abc')
         await ctx.send(f'{round(self.bot.latency*1000)} ms')
 
 def setup(bot):
